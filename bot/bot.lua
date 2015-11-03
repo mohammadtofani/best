6281b0c85f9f09ecc8b3d94e76865e2d6dd68351
ArmanIr <memfi4@yahoo.com>
2015-11-03 20:08:23 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = 102490691},
+    sudo_users = 102490691,51718050},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
