75719f674492d22666af59d33dafebcd0931e2b4
ArmanIr <memfi4@yahoo.com>
2015-11-07 17:01:49 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -4,12 +4,12 @@ function run(msg, matches)
   return 'Telegram Bot '.. VERSION .. [[ 
   Checkout http://git.io/v8qW7
   GNU GPL v2 license.
-  @ThisIsArman برای اطلاعات بیشتر به یوزر سازنده مراجعه کنید]]
+  @ThisIsArman برای اطلاعات بیشتر به یوزر سازنده مراجعه کنید a]]
 end
 
 return {
-  description = "نـمایــش سـورسـ بـات", 
-  usage = "!version: نـمایــش سـورسـ بـات"",
+  description = " نـمایــش سـورسـ بـات" , 
+  usage = "!version: نـمایــش سـورسـ بـات "",
   patterns = {
     "^!version$"
   }, 
