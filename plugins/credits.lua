1edc61a032196e600b6a1a9de68f7e869c00638b
ArmanIr <memfi4@yahoo.com>
2015-11-07 21:56:36 +0330

    Update credits.lua

--- plugins/credits.lua
+++ plugins/credits.lua
@@ -1,15 +1,15 @@
 do
 
 function run(msg, matches)
-  return '@ThisArman '.. credits .. [[ 
+  return ' @ThisArman '.. credits .. [[ 
   ایـن بــاتـ تـوسطـ @ThisIsArmanسـاخته شده است
   تـرجمهـ سـورسـ نـیز تـوسطـ او صورتـ گـرفتـ
-  کپـیـ ایـن سورسـ بدون اطلاعـ سازندهـ حرام اسـت]]
+  کپـیـ ایـن سورسـ بدون اطلاعـ سازندهـ حرام اسـت ]]
 end
 
 return {
   description = " نمـاشـ اطلاعاتـ باتـ , 
-  usage = "!version: نمـاشـ اطلاعاتـ باتـ ",
+  usage = "!credits: نمـاشـ اطلاعاتـ باتـ ",
   patterns = {
     "^!credits$"
   }, 
