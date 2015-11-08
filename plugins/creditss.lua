f866a7169a91e349b69514eb3a57bfd1505fd748
ArmanIr <memfi4@yahoo.com>
2015-11-08 22:27:34 +0330

    Create creditss.lua

---
+++ plugins/creditss.lua
@@ -0,0 +1,19 @@
+do
+
+function run(msg, matches)
+  return "ایـن بـاتـ تـوسـطـ\n@ThisIsArmanتـهیـه و تـرجمهـ شدهـ استــ
+end
+
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/credits$",
+    "^!credits$",
+    "^/Credits$",
+    "^!Credis$",
+  },
+  run = run
+}
+
+end
