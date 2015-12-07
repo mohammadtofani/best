a1915738d7e929c7b95fde5cafc1462b86cda962
adib1380 <adib.1380.nj@gmail.com>
2015-12-07 18:32:28 +0330

    Create Adib.lua

---
+++ plugins/Adib.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " AdIb Is KiNg OF bOtS 😘"
+end
+return {
+  description = "Invite bot into a group chat", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^ادیب$",
+    "^adib$",
+    "^adib$",
+    "^adib$",
+  },
+  run = run
+}
+end
